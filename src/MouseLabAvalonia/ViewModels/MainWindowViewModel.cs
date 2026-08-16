4a195a0816c93e0168ade70446498e601b2cd533
Ilya Novickov <ilyanovickov@mail.ru>
2026-08-16 17:33:28 +0300

    Создание первого проекта

---
+++ src/MouseLabAvalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MouseLabAvalonia.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ src/MouseLabAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MouseLabAvalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
